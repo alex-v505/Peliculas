f6bd988df641b752f57c555e49f4bc3a0331c9c2
Hernan Iturralde <hernan200212itu@gmail.com>
2022-08-14 14:43:31 -0500

    socios view.php finalizado

---
+++ views/socios/view.php
@@ -0,0 +1,42 @@
+<?php include '../template/header.php' ?>
+
+<section class="content">
+    <div>
+        <H3>Datos de socio</H3>
+        <br><br>
+        <div class="container">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col"></th>
+                    <th scope="col">Datos</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">Nombre</th>
+                    <td></td>
+                <tr>
+                    <th scope="row">Cedula</th>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th scope="row">Direccion</th>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th scope="row">Telefono</th>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th scope="row">Correo</th>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+        </div>
+
+    </div>
+
+</section>
+<?php include '../template/footer.php' ?>
