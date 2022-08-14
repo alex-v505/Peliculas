48771cc08ee0a7249a1cdbb0a1ffc36708234c7e
Hernan Iturralde <hernan200212itu@gmail.com>
2022-08-14 13:19:03 -0500

    Pagina delete tabla agregada

---
+++ views/socios/delete.php
@@ -0,0 +1,42 @@
+<?php include '../template/header.php' ?>
+
+<section class="content">
+    <div>
+        <H3>Eliminar socio</H3>
+        <br><br>
+        <div class="container">
+        <table class="table table-striped table-dark">
+            <thead>
+                <tr>
+                    <th scope="col"></th>
+                    <th scope="col">Datos</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">Nombre</th>
+                    <td>Julio Jaramillo</td>
+                <tr>
+                    <th scope="row">Cedula</th>
+                    <td>0503634974</td>
+                </tr>
+                <tr>
+                    <th scope="row">Direccion</th>
+                    <td>Latacunga</td>
+                </tr>
+                <tr>
+                    <th scope="row">Telefono</th>
+                    <td>0958775282</td>
+                </tr>
+                <tr>
+                    <th scope="row">Correo</th>
+                    <td>jotajota@gmail.com</td>
+                </tr>
+            </tbody>
+        </table>
+        </div>
+        
+    </div>
+
+</section>
+<?php include '../template/footer.php' ?>
