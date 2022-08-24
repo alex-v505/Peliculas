c09c9119c103ef7e5ab276e771525a274a60cbe6
Alex V <alexmva03@hotmail.com>
2022-08-24 18:12:44 -0500

    Creacion del dashboard

--- controller/dashboard/index.php
+++ controller/dashboard/index.php
@@ -23,18 +23,35 @@
     //Meses de alquileres
     include '../../model/conectar.php';
     $sql = "SET lc_time_names = 'es_ES'; ";
-    $sql_mes = "SELECT DISTINCT MONTHNAME(alq_fecha_desde) AS mes FROM alquileres;";
+    $sql_mes = "SELECT DISTINCT MONTHNAME(alq_fecha_desde) AS mes FROM alquileres ORDER BY MONTH(alq_fecha_desde) ;";
     $result = $conn->query($sql);
     $result_mes = $conn->query($sql_mes);
     $i = 0; 
+    $total_mes=array();
     if ($result_mes->num_rows > 0) {          
         while($row = $result_mes->fetch_assoc()) {
-            $total_mes[$i]=($row['mes']);
-            $total_mes_js[$i] = json_decode($total_mes[$i]);
+            $total_mes[$i]=("'".$row['mes']."'");
             $i++;
         }
     }
-
-    print_r($total_mes);
+    $meses = "['',".implode(",",$total_mes)."]";
+    //Suma de alquileres por mes
+    $sql_alq_mes = "SELECT SUM(alq_valor) as suma FROM alquileres GROUP BY MONTH(alq_fecha_desde) ORDER BY MONTH(alq_fecha_desde) ;";
+    $result = $conn->query($sql);
+    $result_alq_mes = $conn->query($sql_alq_mes);
+    $i = 0; 
+    $total_alq_mes=array();
+    if ($result_alq_mes->num_rows > 0) {          
+        while($row = $result_alq_mes->fetch_assoc()) {
+            $total_alq_mes[$i]=("'".$row['suma']."'");
+            $i++;
+        }
+    }
+    $alq_meses = "['0',".implode(",",$total_alq_mes)."]";
+    //Suma de alquileres
+    $sql_alq = "SELECT SUM(alq_valor) as suma FROM alquileres;";
+    $result_alq = $conn->query($sql_alq);
+    $row = $result_alq->fetch_assoc();
+    $total_alquileres = $row['suma'];
     include '../../model/desconectar.php';
 ?>
--- view/dashboard/index.php
+++ view/dashboard/index.php
@@ -2,9 +2,9 @@
 <?php include '../../controller/dashboard/index.php' ?>
 <section class="content">
     <div class="row">
-        <div class="col-12 col-sm-6 col-md-3">
+        <div class="col-12 col-sm-6 col-md-4">
             <div class="info-box">
-                <span class="info-box-icon bg-info elevation-1"><i class="fas fa-cog"></i></span>
+                <span class="info-box-icon bg-info elevation-1"><i class="fas fa-film"></i></span>
 
                 <div class="info-box-content">
                     <span class="info-box-text">Total Películas</span>
@@ -17,24 +17,13 @@
             <!-- /.info-box -->
         </div>
         <!-- /.col -->
-        <div class="col-12 col-sm-6 col-md-3">
-            <div class="info-box mb-3">
-                <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-thumbs-up"></i></span>
-
-                <div class="info-box-content">
-                    <span class="info-box-text">Total socios</span>
-                    <span class="info-box-number"><?php echo $total_soc ?></span>
-                </div>
-                <!-- /.info-box-content -->
-            </div>
-            <!-- /.info-box -->
-        </div>
+        
         <!-- /.col -->
 
         <!-- fix for small devices only -->
         <div class="clearfix hidden-md-up"></div>
 
-        <div class="col-12 col-sm-6 col-md-3">
+        <div class="col-12 col-sm-6 col-md-4">
             <div class="info-box mb-3">
                 <span class="info-box-icon bg-success elevation-1"><i class="fas fa-shopping-cart"></i></span>
 
@@ -49,7 +38,7 @@
             <!-- /.info-box -->
         </div>
         <!-- /.col -->
-        <div class="col-12 col-sm-6 col-md-3">
+        <div class="col-12 col-sm-6 col-md-4">
             <div class="info-box mb-3">
                 <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-users"></i></span>
 
@@ -63,26 +52,18 @@
         </div>
         <!-- /.col -->
     </div>
-    <div class="row">
-        <div class="col-lg-6">
-            <div class="card">
-                <div class="card-header border-0">
-                    <div class="d-flex justify-content-between">
-                        <h3 class="card-title">Sales</h3>
-                        <a href="javascript:void(0);">View Report</a>
+    <div class="row justify-content-center">
+        <div class="col-lg-8 ">
+            <div class="card ">
+                <div class="card-header border-0 ">
+                    <div class="d-flex justify-content-center">
+                        <H3 class="card-title text-bold text-lg">ALQUILERES</H3>
                     </div>
                 </div>
                 <div class="card-body">
-                    <div class="d-flex">
+                    <div class="d-flex ">
                         <p class="d-flex flex-column">
-                            <span class="text-bold text-lg">$18,230.00</span>
-                            <span>Sales Over Time</span>
-                        </p>
-                        <p class="ml-auto d-flex flex-column text-right">
-                            <span class="text-success">
-                                <i class="fas fa-arrow-up"></i> 33.1%
-                            </span>
-                            <span class="text-muted">Since last month</span>
+                            <span class="text-bold text-lg ">Total: $<?php echo $total_alquileres?></span>
                         </p>
                     </div>
                     <!-- /.d-flex -->
@@ -90,73 +71,34 @@
                     <div class="position-relative mb-4">
                         <canvas id="sales-chart" height="200"></canvas>
                     </div>
-
-                    <div class="d-flex flex-row justify-content-end">
-                        <span class="mr-2">
-                            <i class="fas fa-square text-primary"></i> This year
-                        </span>
-
-                        <span>
-                            <i class="fas fa-square text-gray"></i> Last year
-                        </span>
-                    </div>
-                </div>
-            </div>
-            <!-- /.card -->
-
-            <div class="card">
-                <div class="card-header border-0">
-                    <h3 class="card-title">Online Store Overview</h3>
-                    <div class="card-tools">
-                        <a href="#" class="btn btn-sm btn-tool">
-                            <i class="fas fa-download"></i>
-                        </a>
-                        <a href="#" class="btn btn-sm btn-tool">
-                            <i class="fas fa-bars"></i>
-                        </a>
-                    </div>
-                </div>
-                <div class="card-body">
-                    <div class="d-flex justify-content-between align-items-center border-bottom mb-3">
-                        <p class="text-success text-xl">
-                            <i class="ion ion-ios-refresh-empty"></i>
-                        </p>
-                        <p class="d-flex flex-column text-right">
-                            <span class="font-weight-bold">
-                                <i class="ion ion-android-arrow-up text-success"></i> 12%
-                            </span>
-                            <span class="text-muted">CONVERSION RATE</span>
-                        </p>
-                    </div>
-                    <!-- /.d-flex -->
-                    <div class="d-flex justify-content-between align-items-center border-bottom mb-3">
-                        <p class="text-warning text-xl">
-                            <i class="ion ion-ios-cart-outline"></i>
-                        </p>
-                        <p class="d-flex flex-column text-right">
-                            <span class="font-weight-bold">
-                                <i class="ion ion-android-arrow-up text-warning"></i> 0.8%
-                            </span>
-                            <span class="text-muted">SALES RATE</span>
-                        </p>
-                    </div>
-                    <!-- /.d-flex -->
-                    <div class="d-flex justify-content-between align-items-center mb-0">
-                        <p class="text-danger text-xl">
-                            <i class="ion ion-ios-people-outline"></i>
-                        </p>
-                        <p class="d-flex flex-column text-right">
-                            <span class="font-weight-bold">
-                                <i class="ion ion-android-arrow-down text-danger"></i> 1%
-                            </span>
-                            <span class="text-muted">REGISTRATION RATE</span>
-                        </p>
-                    </div>
-                    <!-- /.d-flex -->
                 </div>
             </div>
         </div>
     </div>
-
 </section>
+<script>
+  const labels = <?php echo $meses ?>
+
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'Alquileres',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: <?php echo $alq_meses ?>,
+    }]
+  };
+
+  const config = {
+    type: 'line',
+    data: data,
+    options: {}
+  };
+</script>
+<script>
+  const myChart = new Chart(
+    document.getElementById('sales-chart'),
+    config
+  );
+</script>
 <?php include '../template/footer.php' ?>
